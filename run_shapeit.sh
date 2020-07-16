a3e1a5452b655e0165ad053766406941ea2a9205
GregoryMatesi <44989448+GregoryMatesi@users.noreply.github.com>
2020-07-15 21:29:28 -0600

    modify for BDC

--- run_shapeit.sh
+++ run_shapeit.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
 nr_threads=8
-shapeit_dir=/home/test/shapeit_input/genetic_map_hapmap/chr22.txt
+#shapeit_dir=/home/test/shapeit_input/genetic_map_hapmap/chr22.txt
+#file=/home/test/bard/BARD_chr22
 
-file=/home/test/bard/BARD_chr22
+#/sbgenomics/Projects/d7973597-4d40-4cd3-85d3-017daccc01a9/BARD_chr22.bed=$1
+#/sbgenomics/workspaces/d7973597-4d40-4cd3-85d3-017daccc01a9/tasks/38bc33a0-1521-453b-99a2-6c7e2e060fa3/sbg_decompressor_cwl1_0/decompressed_files/chr22.txt=$2
+
+
+shapeit_dir= $2
+file = $1
+
 
 # get chr from file
 chr=`basename $file | cut -f2 -d'_' | sed 's/chr//'`
