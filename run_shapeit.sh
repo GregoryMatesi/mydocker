8cba412a9e84b4bcb7058449c3af91c480c82075
GregoryMatesi <44989448+GregoryMatesi@users.noreply.github.com>
2020-07-28 13:12:56 -0600

    shapeit_dir is last input

--- run_shapeit.sh
+++ run_shapeit.sh
@@ -10,9 +10,9 @@ nr_threads=8
 
 
 
-file = $1
-shapeit_dir = $2
 
+shapeit_dir = $1
+file = $4
 #shapeit_dir = /sbgenomics/workspaces/d7973597-4d40-4cd3-85d3-017daccc01a9/tasks/38bc33a0-1521-453b-99a2-6c7e2e060fa3/sbg_decompressor_cwl1_0/decompressed_files/chr22.txt
 
 # I think 'file' is OK. But 'shapeit_dir' is currently every chromosome's txt file in the tar ball. 
