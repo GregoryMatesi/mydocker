53c82246127eaef709dc5e4fe4555ebfcf06c7e7
GregoryMatesi <44989448+GregoryMatesi@users.noreply.github.com>
2020-08-10 14:10:29 -0600

    new release with echo plink_file

--- run_shapeit.sh
+++ run_shapeit.sh
@@ -15,6 +15,8 @@ echo "Name of bed_file is $bed_file" 1>&2
 ## plink needs the the file name without an extension as input
 plink_file=`sed 's/.bed//' $bed_file`
 
+echo "Name of plink file is $plink_file" 1>&2
+
 # get chr from file
 chr=`basename $plink_file | cut -f2 -d'_' | sed 's/chr//'`
 
@@ -28,12 +30,13 @@ plink --bfile ${plink_file} \
       --extract ../test/chr${chr}_snps.txt \
       --make-bed --out ../test/chr${chr}
 
+# comment out shapeit to run locally
 # Run shapeit.
-shapeit --input-bed  /home/test/chr${chr}.bed \
-        /home/test/chr${chr}.bim \
-        /home/test/chr${chr}.fam \
-        --input-map ${shapeit_dir}/genetic_map_hapmap/ \
-        --thread ${nr_threads} \
-        --output-max  chr${chr}.haps \
-        /home/test/chr${chr}.samples
+#shapeit --input-bed  /home/test/chr${chr}.bed \
+#        /home/test/chr${chr}.bim \
+#        /home/test/chr${chr}.fam \
+#        --input-map ${shapeit_dir}/genetic_map_hapmap/ \
+#        --thread ${nr_threads} \
+#        --output-max  chr${chr}.haps \
+#        /home/test/chr${chr}.samples
 
