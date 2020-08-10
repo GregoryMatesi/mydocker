72cda95f9d0c9a0896245aa1a492db545c5ccefd
GregoryMatesi <44989448+GregoryMatesi@users.noreply.github.com>
2020-08-10 14:30:18 -0600

    adding $ to variables

--- run_shapeit.sh
+++ run_shapeit.sh
@@ -4,16 +4,16 @@
 nr_threads=$1
 
 #input parameters
-bed_file=$2
-fam_file=$3
-bim_file=$4
-shapeit_dir=$5
-shapeit_script=$6
+$bed_file=$2
+$fam_file=$3
+$bim_file=$4
+$shapeit_dir=$5
+$shapeit_script=$6
 
 echo "Name of bed_file is $bed_file" 1>&2
 
 ## plink needs the the file name without an extension as input
-plink_file=`sed 's/.bed//' $bed_file`
+$plink_file=`sed 's/.bed//' $bed_file`
 
 echo "Name of plink file is $plink_file" 1>&2
 
